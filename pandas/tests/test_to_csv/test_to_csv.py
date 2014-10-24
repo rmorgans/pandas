b3bb86ba39dcc2de29ece0786f57328f283af53e
Rick Morgans <rick.morgans@gmail.com>
2014-10-25 00:42:53 +1030

    updated function name

--- pandas/tests/test_to_csv/test_to_csv.py
+++ pandas/tests/test_to_csv/test_to_csv.py
@@ -7,6 +7,6 @@ def test_to_csv_works():
     pd.DataFrame(np.random.randn(3, 100000)).to_csv('test.csv')
     assert True
 
-def test_to_csv_works():
+def test_to_csv_doesnt_work():
     pd.DataFrame(np.random.randn(3, 100001)).to_csv('test.csv')
     assert True
