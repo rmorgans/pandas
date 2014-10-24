674be2d9df342de55e1141f60ed117a2ccaad8d8
Rick Morgans <rick.morgans@gmail.com>
2014-10-25 00:35:06 +1030

    change to random matrix

--- pandas/tests/test_to_csv/test_to_csv.py
+++ pandas/tests/test_to_csv/test_to_csv.py
@@ -3,10 +3,10 @@
 
 import pandas as pd
 
+def test_to_csv_works():
+    pd.DataFrame(np.random.randn(3, 100000)).to_csv('test.csv')
+    assert True
 
-def test_to_csv():
-    import gzip
-    f = gzip.open('to_csv_test.msgpack.gz', 'rb')
-    d=pd.read_msgpack(f)
-    d.to_csv('test.csv')
+def test_to_csv_works():
+    pd.DataFrame(np.random.randn(3, 100001)).to_csv('test.csv')
     assert True
